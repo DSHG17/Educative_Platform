82ea6a8354649fcfff6f4a02049310329bf678cb
DSHG <tohsakaringod@gmail.com>
2025-02-05 11:18:17 -0600

    add: i add the carpet src with some shcemas for the platform

---
+++ src/course/course.model.js
@@ -0,0 +1,23 @@
+import { Schema, model } from "mongoose";
+
+courseSchema = Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    teacher:{
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    students:[{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+    
+
+})
